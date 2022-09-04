25427ac89cae2e28e2965d1d5b79274b3d75230d
DSouLima <davi.souza.lima@outlook.com>
2022-09-04 14:36:00 -0300

    Third commit

--- ex3.py
+++ ex3.py
@@ -4,4 +4,4 @@ numero = int(input("Digite um numero inteiro"))
 print(f"Tabuada do numero {numero}")
 
 for i in range (1,11):
-    print(numero, "x", i, "=", (numero * i))
+    print(numero, "x", i, "=", (numero * i)) 
